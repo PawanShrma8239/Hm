d2b421efa1b14605647b41d3219472237a8d8d05
PawanShrma8239 <pawansharma.marolix@gmail.com>
2023-12-06 16:33:42 +0530

    login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' }, // Redirect empty path to '/home'
-{ path: 'main', component: MainComponent },];
+{ path: 'main', component: MainComponent },
+{path:'login', component:LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
