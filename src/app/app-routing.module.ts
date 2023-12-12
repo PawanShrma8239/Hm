5ea08939f6fd00495d550ec4601aeee91e56094f
Nazeer <nazeer.marolix@gmail.com>
2023-12-12 10:37:10 +0530

    adding feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
+import { FeatureComponent } from './feature/feature.component';
 
 
 
 
 
 const routes: Routes = [
- { path: '', redirectTo: 'main', pathMatch: 'full' }, // Redirect empty path to '/home'
+ { path: '', redirectTo: 'feature', pathMatch: 'full' }, // Redirect empty path to '/home'
 { path: 'main', component: MainComponent },
 {path:'login', component:LoginComponent},
-
+{path:'feature', component:FeatureComponent},
 
 
 
--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -5,11 +5,15 @@ import { FeatureComponent } from './feature.component';
 import { RouterModule } from '@angular/router';
 import { FeatureRoutingModule } from './feature-routing.module';
 
+import { ReportComponent } from './report/report.component';
+
 
 
 @NgModule({
   declarations: [
     FeatureComponent,
+   
+    ReportComponent,
    
   ],
   imports: [
