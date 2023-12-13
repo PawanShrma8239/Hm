daf8ce21cf4ad39f9802ea621d2b20c01ef6a068
PawanShrma8239 <pawansharma.marolix@gmail.com>
2023-12-13 14:50:55 +0530

    display massage notification etc popup

--- src/app/feature/fheader/fheader.component.ts
+++ src/app/feature/fheader/fheader.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./fheader.component.css']
 })
 export class FheaderComponent {
+  isPDropdownOpen = false;
+  isMDropdownOpen = false;
+  isNDropdownOpen = false;
+  isLDropdownOpen = false;
 
+  togglePDropdown() {
+    this.closeAllMenusExcept('admin');
+    this.isPDropdownOpen = !this.isPDropdownOpen;
+  }
+  toggleMDropdown() {
+    this.closeAllMenusExcept('message');
+    this.isMDropdownOpen = !this.isMDropdownOpen;
+  }
+  toggleNDropdown() {
+    this.closeAllMenusExcept('notification');
+    this.isNDropdownOpen = !this.isNDropdownOpen;
+  }
+  toggleLDropdown() {
+    this.closeAllMenusExcept('language');
+    this.isLDropdownOpen = !this.isLDropdownOpen;
+  }
+  private closeAllMenusExcept(menuToKeepOpen: string) {
+    if (menuToKeepOpen !== 'admin') {
+      this.isPDropdownOpen = false;
+    }
+    if (menuToKeepOpen !== 'message') {
+      this.isMDropdownOpen = false;
+    }
+    if (menuToKeepOpen !== 'notification') {
+      this.isNDropdownOpen = false;
+    }
+    if (menuToKeepOpen !== 'language') {
+      this.isLDropdownOpen = false;
+    }
+  }
 }
